582efbcac1b1fa122e46fe642db75e71b69dbd4d
Prathamesh Kotwal <prathameshk0405@gmail.com>
2026-06-03 15:44:43 +0530

    Added further fuctinality for jpa classes with h2 db

--- src/main/java/com/ecom/app/model/User.java
+++ src/main/java/com/ecom/app/model/User.java
@@ -1,10 +1,42 @@
 package com.ecom.app.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+//@AllArgsConstructor
+@Entity(name = "user_table")
+//@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+//    @Column
     private String firstName;
+
+//    @Column
     private String lastName;
+
+    private String email;
+
+    private String phone;
+
+     private UserRole userRole = UserRole.CUSTOMER;
+
+     @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+     @JoinColumn(name = "address_id", referencedColumnName = "id")
+     private Address address;
+
+     @CreationTimestamp
+     private LocalDateTime createdAt;
+
+     @UpdateTimestamp
+     private LocalDateTime updatedAt;
 }
--- src/main/java/com/ecom/app/service/UserService.java
+++ src/main/java/com/ecom/app/service/UserService.java
@@ -1,36 +1,37 @@
 package com.ecom.app.service;
 
 import com.ecom.app.model.User;
+import com.ecom.app.repository.UserRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
 
-    List<User> userList = new ArrayList<>();
-    private Long nextId = 1L;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
 
     public List<User> fetchAllUsers(){
-        return userList;
+        return userRepository.findAll();
     }
 
     public void addUser( User user){
-        user.setId(nextId++);
-        userList.add(user);
+        userRepository.save(user);
     }
 
     public Optional<User> fetchUser(Long id) {
-        return userList.stream().filter(user -> user.getId().equals(id)).findFirst();
+        return userRepository.findById(id);
     }
 
     public boolean updateUser(Long id, User updatedUser){
-        return userList.stream()
-                .filter(user -> user.getId().equals(id)).findFirst()
+        return userRepository.findById(id)
                 .map(existingUser -> {existingUser.setFirstName(updatedUser.getFirstName());
                                             existingUser.setLastName(updatedUser.getLastName());
+                                            userRepository.save(existingUser);
                                             return true;}).orElse(false);
     }
 
