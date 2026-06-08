8fc36ff39b51a9debfdbeeebed316f76293d5a3c
Prathamesh Kotwal <prathameshk0405@gmail.com>
2026-06-08 23:05:39 +0530

    Added Products to the service

---
+++ src/main/java/com/ecom/app/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.ecom.app.controller;
+
+import com.ecom.app.dto.ProductRequest;
+import com.ecom.app.dto.ProductResponse;
+import com.ecom.app.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest productRequest){
+        return new ResponseEntity<ProductResponse>(productService.createProduct(productRequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id,@RequestBody ProductRequest productRequest){
+        return productService.updateProduct(id ,productRequest).map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+}
---
+++ src/main/java/com/ecom/app/service/ProductService.java
@@ -0,0 +1,54 @@
+package com.ecom.app.service;
+
+import com.ecom.app.dto.ProductRequest;
+import com.ecom.app.dto.ProductResponse;
+import com.ecom.app.model.Product;
+import com.ecom.app.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = new Product();
+        updateProductFromRequest(product, productRequest);
+        Product savedProduct = productRepository.save(product);
+        return mapToProductResponse(savedProduct);
+    }
+
+    private ProductResponse mapToProductResponse(Product savedProduct) {
+        ProductResponse response = new ProductResponse();
+        response.setName(savedProduct.getName());
+        response.setDescription(savedProduct.getDescription());
+        response.setCategory(savedProduct.getCategory());
+        response.setPrice(savedProduct.getPrice());
+        response.setImageUrl(savedProduct.getImageUrl());
+        response.setId(savedProduct.getId());
+        response.setStockQuantity(savedProduct.getStockQuantity());
+        response.setActive(savedProduct.getActive());
+        return response;
+    }
+
+    private void updateProductFromRequest(Product product, ProductRequest productRequest) {
+        product.setName(productRequest.getName());
+        product.setDescription(productRequest.getDescription());
+        product.setPrice(productRequest.getPrice());
+        product.setCategory(productRequest.getCategory());
+        product.setImageUrl(productRequest.getImageUrl());
+        product.setStockQuantity(productRequest.getStockQuantity());
+    }
+
+    public Optional<ProductResponse> updateProduct(Long id, ProductRequest productRequest) {
+        return productRepository.findById(id).map(existingProduct -> {
+            updateProductFromRequest(existingProduct, productRequest);
+            Product savedProduct = productRepository.save(existingProduct);
+            return mapToProductResponse(savedProduct);
+        });
+    }
+}
